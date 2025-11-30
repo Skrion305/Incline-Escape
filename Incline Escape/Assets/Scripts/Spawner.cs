cb695690e7be2389653d9e9d50940db830cad285
Kirill Sirotkin <kirasir2006@gmail.com>
2025-11-30 22:49:15 +0300

    Spawn

--- Incline Escape/Assets/Scripts/Spawner.cs
+++ Incline Escape/Assets/Scripts/Spawner.cs
@@ -1,16 +1,56 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] ARRaycastManager raycastManager;
+    [SerializeField] InputActionReference tapAction;
+    [SerializeField] GameObject maze;
+    GameObject spawnedObject;
+    private void OnEnable()
     {
-        
+        tapAction.action.Enable();
+        tapAction.action.performed += OnTap;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        tapAction.action.Disable();
+        tapAction.action.performed -= OnTap;
+    }
+    private void OnTap(InputAction.CallbackContext context)
+    {
+        Vector2 touchPosition;
+        if (context.control is Pointer inputPointer)
+        {
+            touchPosition = inputPointer.position.ReadValue();
+        }
+        else if (context.control is TouchControl pointerControl)
+        {
+            touchPosition = pointerControl.position.ReadValue();
+        }
+        else
+        {
+            touchPosition = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        }
+        var arRaycastHits = new List<ARRaycastHit>();
+        if (!raycastManager.Raycast(touchPosition, arRaycastHits, TrackableType.PlaneWithinPolygon))
+        {
+            return;
+        }
+        var hitPose = arRaycastHits[0].pose;
+        if (spawnedObject != null)
+        {
+            spawnedObject.transform.position = hitPose.position;
+            spawnedObject.transform.rotation = hitPose.rotation;
+        }
+        else
+        {
+            spawnedObject = Instantiate(maze, hitPose.position, hitPose.rotation);
+        }
     }
 }
