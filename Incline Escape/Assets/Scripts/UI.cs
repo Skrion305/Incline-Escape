e3589018bb475cef80dd0e910cfe2156c5713899
Kirill Sirotkin <kirasir2006@gmail.com>
2025-12-01 02:15:40 +0300

    UI

---
+++ Incline Escape/Assets/Scripts/UI.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] GameObject xrOrigin;
+    [SerializeField] GameObject arSession;
+    [SerializeField] MonoBehaviour[] scripts;
+    [SerializeField] GameObject menu;
+    [SerializeField] GameObject pause;
+    [SerializeField] GameObject game;
+    void SetSystemsActive(bool active)
+    {
+        foreach (var s in scripts)
+        {
+            if (s != null)
+            {
+                s.enabled = active;
+            }
+        }
+        if (xrOrigin != null)
+        {
+            xrOrigin.SetActive(active);
+        }
+        if (arSession != null)
+        {
+            arSession.SetActive(active);
+        }
+    }
+    public void Menu()
+    {
+        menu.SetActive(true);
+        game.SetActive(false);
+        pause.SetActive(false);
+        Time.timeScale = 1f;
+        SetSystemsActive(false);
+    }
+    public void StartGame()
+    {
+        menu.SetActive(false);
+        game.SetActive(true);
+        pause.SetActive(false);
+        Time.timeScale = 1f;
+        SetSystemsActive(true);
+    }
+    public void Pause()
+    {
+        menu.SetActive(false);
+        game.SetActive(false);
+        pause.SetActive(true);
+        Time.timeScale = 0f;
+        SetSystemsActive(false);
+    }
+    public void ResumeGame()
+    {
+        menu.SetActive(false);
+        game.SetActive(true);
+        pause.SetActive(false);
+        Time.timeScale = 1f;
+        SetSystemsActive(true);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
